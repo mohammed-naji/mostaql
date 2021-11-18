b679d2f02de1cd8ba5df46e3f31b07efb78e7c04
Mohammed <m.naji1512@gmail.com>
2021-11-18 10:17:55 +0200

    design

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # This code belongsTo Mohammed Naji
 ## For any request please contact me via Facebook
 <a href="#">Facebook</a>
-- [Facebook](https://facebook.com).
+[Facebook](https://facebook.com).
--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,12 @@
 <?php
 
+use App\Http\Controllers\Front\SiteController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [SiteController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
 
-// Auth::attempt(['email' => $email, 'password' => $password]);
-// $user;
-// Auth::login($user);
-// Auth::logout();
-// Hash::make('123');
-// bcrypt('123');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
